f14c13d54a1934a685bd912cde4b8cb1ea953549
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2023-08-27 18:16:02 -0400

    Update move.js

--- scripts/move.js
+++ scripts/move.js
@@ -3,6 +3,10 @@ import {Piece, Pawn, Rook, Bishop, Queen, King, Knight, Empty} from "./piece.js"
 import {Game} from "./game.js";
 import {Player, AI} from "./player.js";
 
+// The move class is a data class that stores the start, 
+// end, and piece type calues. The exception is 
+// for castling and en passant. The type notes whether 
+// or not the move can perform a capture (pawns cant capture forward).
 export class Move {
     constructor(piece, start, end, exception) {
         this.piece = piece;
